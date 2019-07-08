3ce1cdd1b330297fc3e6eb64e58b6da6c445755a
Alex Wright <jawright94@gmail.com>
2019-07-08 12:13:01 -0400

    please make a conflict

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 const quotes = {
     0 : '“An eye for an eye will only make the whole world blind.” ― Mahatma Gandhi',
     1 : '“All war is a symptom of mans failure as a thinking animal.”― John Steinbeck',
-    2 : '“Do not think of knocking out another persons brains because he differs in opinion from you. It would be as rational to knock yourself on the head because you differ from yourself ten years ago.” ― Horace Mann',
+    2 : '“Success is not final, failure is not fatal” ― Winston Churchill',
     3: '“If somebody offers you an amazing opportunity but you are not sure you can do it, say yes – then learn how to do it later!” ― Richard Branson'
 }
 
